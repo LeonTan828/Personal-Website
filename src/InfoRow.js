052d957a0c68992b3db6a52a4c89eb6df9a02693
Leon <yltan828@gmail.com>
2019-09-26 02:02:40 -0500

    added text for about me

--- src/InfoRow.js
+++ src/InfoRow.js
@@ -14,20 +14,34 @@ class InfoRow extends React.Component {
                 <Card className="InfoRow-Card Card-Left">
                     <Image className="InfoRow-Card-Image" src={PATH_TO_IMG} fluid />
                     <Card.Body className="InfoRow-Card-Content">
-                        <Card.Title>About Me</Card.Title>
+                        <Card.Title className="Card-Title">About Me</Card.Title>
                         <Card.Text>
-                            With supporting text below as a natural lead-in to additional content.
+                            My name is Leon Tan and I was born and raised in Kuala Lumpur, Malaysia.
+                            Kuala Lumpur is a unique and vibrant multicultural city in South East Asia
+                            and has definitely played an important role in shaping who I am today.
+                            I am glad to have grew up there as it has allowed me to learn and interact with
+                            people from different background and identity; something that I realized
+                            not many people have the privilege to experience.
                         </Card.Text>
-                        <Button variant="primary">Find out more!</Button>
+                        <Card.Text>
+                            On August of 2015, I came to the United States to pursue my Undergraduate degree.
+                            I ended up in Madison, Wisconsin and has been living here for more than 4 years.
+                            Coming abroad has been the best decision that I have made yet.
+                            This is because being so far from home and out of my comfort zone has
+                            motivated me to try new things and expand on my experiences.
+                            In fact, I have since experienced a huge personal growth and became a better
+                            person that I was.
+                        </Card.Text>
+                        {/* <Button variant="primary">Find out more!</Button> */}
                     </Card.Body>
                 </Card>
                 <Card className="InfoRow-Card Card-Right">
                     <Card.Body className="InfoRow-Card-Content">
-                        <Card.Title>Fill in later</Card.Title>
+                        <Card.Title className="Card-Title">Fill in later</Card.Title>
                         <Card.Text>
                             With supporting text below as a natural lead-in to additional content.
                         </Card.Text>
-                        <Button variant="primary">Find out more!</Button>
+                        {/* <Button variant="primary">Find out more!</Button> */}
                     </Card.Body>
                     <Image className="InfoRow-Card-Image" src={PATH_TO_IMG} fluid />
                 </Card>
